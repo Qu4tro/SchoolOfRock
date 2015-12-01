05ea5412eded7b2b6d5822d538849d71fab93096
7patricia <bpatrcia@gmail.com>
2015-12-01 08:21:10 +0000

    added datasets and scripts sql

---
+++ alterarAluno.php
@@ -0,0 +1,24 @@
+
+<?php 
+
+    $dbh = new PDO('mysql:host=localhost;dbname=pri2015','root','root');        
+    $oldId = $_REQUEST['id'];
+    $qstring = "SELECT * FROM Aluno WHERE id='".$oldId."'";  
+    $resultado = $dbh->query($qstring);
+    
+    $aluno = $resultado->fetch(); 
+    
+    print "<form action='alterarAluno2.php' method='post'>";
+    print "<table>";
+    print "<tr><td>Identificador</td><td><input type='text' value='".$aluno['id']."' name='id'></td></tr>";
+    print "<tr><td>Nome</td><td><input type='text' value='".$aluno['nome']."' name='nome'></td></tr>";
+    print "<tr><td>Data de Nascimento</td><td><input type='text' value='".$aluno['dataNasc']."' name='dataNasc'></td></tr>";
+    print "<tr><td>Curso</td><td><input type='text' value='".$aluno['curso']."' name='curso'></td></tr>";
+    print "<tr><td>Ano</td><td><input type='text' value='".$aluno['anoCurso']."' name='anoCurso'></td></tr>";
+    print "<tr><td>Instrumento</td><td><input type='text' value='".$aluno['instrumento']."' name='instrumento'></td></tr>";
+    print "</table>";
+    print "<input type='hidden' value='".$oldId."' name='oldId'>";
+    print "<input type='submit' value='Enviar'>";
+    print "</form>";  
+    
+?>
---
+++ alterarAluno2.php
@@ -0,0 +1,15 @@
+
+<?php 
+
+    $dbh = new PDO('mysql:host=localhost;dbname=pri2015','root','root');        
+    $qstring = "UPDATE Aluno SET id='".$_REQUEST['id']."', nome='".$_REQUEST['nome']."', dataNasc='".$_REQUEST['dataNasc']."', curso='".$_REQUEST['curso']."', anoCurso='".$_REQUEST['anoCurso']."', instrumento='".$_REQUEST['instrumento']."' WHERE id='".$_REQUEST['oldId']."';";
+    print $qstring."<br/>";
+    $resultado = $dbh->query($qstring);
+    
+    if($resultado) 
+    {
+        print "Aluno com id ".$_REQUEST['id']." alterado com sucesso<br/>";
+        print "<a href='gestaoAlunos.php'>Voltar</a>";
+    }
+    
+?>

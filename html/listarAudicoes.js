db02dae9f78f2248b432d2afc24d07bffbf26135
7patricia <bpatrcia@gmail.com>
2016-01-11 20:32:53 +0000

    added pagination and begining filter

--- html/listarAudicoes.js
+++ html/listarAudicoes.js
@@ -1,10 +1,18 @@
 
             var ids= ["A1",
+    "A2",
     ];
             var nomes=["Récita 1º Dezembro",
+    "Atuacao 1",
+    "Atuacao 2",
+    "Récita 1º Dezembro",
+    "Atuacao 1",
+    "Atuacao 2",
     ];
-            var datas = ["1 de Dezembro",
+            var datas = ["2016-12-01",
+    "2015-12-01",
     ];
             var locais = ["Theatro Circo",
+    "Theatro Circo",
     ];
         
--- html/povoarTabelaAudicoes.js
+++ html/povoarTabelaAudicoes.js
@@ -5,3 +5,25 @@ $(document).ready(function(){
         $("#audicoes").append("<tr><td>"+ids[index]+"</td><td>"+nomes[index]+"</td><td>"+datas[index]+"</td><td>"+locais[index]+"</td><td><input type=\"button\" onclick=\"location.href=\'alterarAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Alterar\"/></td><td><input type=\"button\" onclick=\"location.href=\'removerAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Remover\"/></td><td><input type=\"button\" onclick=\"location.href=\'consultarAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Consultar\"/></td></tr>");
     }
 });
+	
+
+
+$("#filtro").live('change',function(){
+	var select = document.getElementById("filtro");
+	var selected = e.options[e.selectedIndex].text;
+
+	if(selected == "Passados"){
+		for(index=0;index<ids.length;index++){
+			if(moment(datas[index]).isBefore(moment())){
+				$("#audicoes").append("<tr><td>"+ids[index]+"</td><td>"+nomes[index]+"</td><td>"+datas[index]+"</td><td>"+locais[index]+"</td><td><input type=\"button\" onclick=\"location.href=\'alterarAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Alterar\"/></td><td><input type=\"button\" onclick=\"location.href=\'removerAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Remover\"/></td><td><input type=\"button\" onclick=\"location.href=\'consultarAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Consultar\"/></td></tr>");
+			}
+		 }
+	}
+	if(selected == "Futuros"){
+		for(index=0;index<ids.length;index++){
+			if(moment(datas[index]).isAfter(moment())){
+				$("#audicoes").append("<tr><td>"+ids[index]+"</td><td>"+nomes[index]+"</td><td>"+datas[index]+"</td><td>"+locais[index]+"</td><td><input type=\"button\" onclick=\"location.href=\'alterarAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Alterar\"/></td><td><input type=\"button\" onclick=\"location.href=\'removerAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Remover\"/></td><td><input type=\"button\" onclick=\"location.href=\'consultarAudicao.php?idAudicao="+ids[index]+"\'\" value=\"Consultar\"/></td></tr>");
+			}
+		 }
+	}
+});

2c93120aa1c4217ad1095dd1b9bce1b69e812f9f
7patricia <bpatrcia@gmail.com>
2016-01-11 17:41:08 +0000

    fixes updates

--- html/alterarAluno.php
+++ html/alterarAluno.php
@@ -13,7 +13,8 @@ echo "<form action='doAlterarAluno.php' class='newForm'>";
 echo "Id: <input type='text' name='id' value='".(string)$aluno[0]['id']."'size='3'> <br/>";
 echo "Nome: <input type='text' name='nome' value='".(string)$aluno[0]->nome."'size='45'> <br/>";
 echo "Data de Nascimento: <input type='text' name='data'value='".(string)$aluno[0]->dataNasc."' size='6'> <br/>";
-echo "Curso: <input type='text' name='curso' value='".(string)$aluno[0]->curso."'size='4'> <br/>";
+echo "Curso: <select name='cursos' id='cursos'><option selected='selected'>".(string)$aluno[0]->curso."</option></select><br/>";
 echo "Ano do Curso: <input type='text' name='ano' value='".(string)$aluno[0]->anoCurso."'size='1'><br/>"; 
+echo "Instrumento: <select name='instrumentos' id='instrumentos'><option selected='selected'>".(string)$aluno[0]->instrumento."</option></select><br/>";
 echo "<center><input type='submit' id='bigButton' value='Alterar'> </center>";
 ?>
--- html/alterarProfessor.php
+++ html/alterarProfessor.php
@@ -14,7 +14,7 @@ echo "<form action='doAlterarProfessor.php' class='newForm'>";
 echo "Id: <input type='text' name='id' value='".(string)$professor[0]['id'	]."'size='3'> <br/>";
 echo "Nome: <input type='text' name='nome' value='".(string)$professor[0]->nome."'size='45'> <br/>";
 echo "Data de Nascimento: <input type='text' name='data' value='".(string)$professor[0]->dataNasc."' size='6'> <br/>";
-echo "Curso: <input type='text' name='curso' value='".(string)$professor[0]->curso."'size='25'> <br/>";
+echo "Curso: <select name='cursos' id='cursos'><option selected='selected'>".(string)$professor[0]->curso."</option></select><br/>";
 echo "Habilitações: <input type='text' name='habil' value='".(string)$professor[0]->habilitacoes."'size='15'><br/>"; 
 echo "<center><input type='submit' id='bigButton' value='Alterar'> </center>";
 ?>

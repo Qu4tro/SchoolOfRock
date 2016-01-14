b4c66598ae7563d0256e2c614a9ebc5e55cf292f
7patricia <bpatrcia@gmail.com>
2016-01-14 10:23:01 +0000

    started import files

--- html/removerAluno.php
+++ html/removerAluno.php
@@ -3,6 +3,9 @@
 	$logs = new DOMDocument(); 
 	$logs->load('alunos.xml');
 
+	$xsl = new DOMDocument;
++	$xsl->load('alunos.xsl');
+
 	$alunos = $logs->getElementsByTagName('aluno');
 
 	foreach ($alunos as $aluno) {
@@ -13,4 +16,9 @@
 
 	$logs->save("alunos.xml");
 	header("Location:alunos.html");
+
+	$proc = new XSLTProcessor;
+	$proc->importStyleSheet($xsl); 
+
+	$proc->transformToXML($logs);
 ?>

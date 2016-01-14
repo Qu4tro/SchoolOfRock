74fc101b89234eefb823d4e6d5d5b6a8d0f63123
7patricia <bpatrcia@gmail.com>
2016-01-14 13:31:19 +0000

    minor fixes

--- html/alterarProfessor.php
+++ html/alterarProfessor.php
@@ -10,11 +10,11 @@ $logs = simplexml_load_file("professores.xml");
 $professor= $logs->xpath("//professor[@id='".$id."']");
 
 echo "<div class='newFormDiv'>";
-echo "<form action='doAlterarProfessor.php' class='newForm'>";
+echo "<form action='doAlterarProfessor.php' onsubmit='validar()'class='newForm'>";
 echo "Id: <input type='text' name='id' value='".(string)$professor[0]['id']."'size='3' readonly> <br/>";
 echo "Nome: <input type='text' name='nome' value='".(string)$professor[0]->nome."'size='45'> <br/>";
 echo "Data de Nascimento: <input type='text' name='data' value='".(string)$professor[0]->dataNasc."' size='6'> <br/>";
 echo "Curso: <select name='cursos' id='cursos'><option selected='selected'>".(string)$professor[0]->curso."</option></select><br/>";
 echo "Habilitações: <input type='text' name='habil' value='".(string)$professor[0]->habilitacoes."'size='15'><br/>"; 
-echo "<center><input type='submit' id='bigButton' onclick=\"validar()\" value='Alterar'> </center>";
+echo "<center><input type='submit' id='bigButton' value='Alterar'> </center>";
 ?>
